312ce59af25ae6569c77e6982c15160104945a40
Alvin Galit <aroblesgalit@gmail.com>
2020-08-12 14:38:40 -0500

    Styled memory match page

--- client/src/pages/MemoryMatch.js
+++ client/src/pages/MemoryMatch.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MemoryMatchCards from '../components/MemoryMatchCards';
+import './pages.scss';
 
 export default function MemoryMatch() {
     return (
-        <>
+        <div className='container memory-match'>
             <h1>Card Memory Matching Game</h1>
             <MemoryMatchCards />
-        </>
+        </div>
     )
 }
