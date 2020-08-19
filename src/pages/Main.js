494b579885c46665b8a7af0191efd141f09c71f8
Alvin Galit <aroblesgalit@gmail.com>
2020-08-19 08:36:09 -0700

    Styled main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './pages.scss';
 
 export default function Main() {
     return (
-        <div>
-            <Link to="/memory-match">Card Flip Animation</Link>
+        <div className='container main'>
+            <h1>React Spring Practice</h1>
+            <Link to="/memory-match" className='items'>Card Flip Animation</Link>
         </div>
     )
 }
